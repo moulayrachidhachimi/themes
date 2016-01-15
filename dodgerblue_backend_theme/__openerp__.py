d5deefcd5e542b6d2cd76af3b35cdff149355f00
Mario Gielissen <mario@gielissen.org>
2016-01-15 22:34:53 +0100

    Initial commit

---
+++ dodgerblue_backend_theme/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name' : 'DodgerBlue Backend Theme',
+    'version' : '0.1.0',
+    'author' : 'Openworx',
+    'category' : 'Backend',
+    'website' : 'http://www.openworx.nl',
+    'description': """
+DodgerBlue Backend Theme for Odoo 8.0.
+    """,
+    'depends' : ['base'],
+    'data':[
+        'views.xml',
+        ],
+    'installable': True
+}
